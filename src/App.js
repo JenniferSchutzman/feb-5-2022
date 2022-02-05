3b1d7697584e8b1419aced24f6f65bfba9ec06bd
Jenny Schutzman <jenny.schutzman@ratequote.com>
2022-02-05 10:48:04 -0500

    store set-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+// import updateMovies from "./store/actions/updateMovies";
 
-function App() {
+function App(props) {
+  console.log("props", props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>REDUX MOVIE LIST APP</div>
+      <div> Your current movie is: {props.movies.name} </div>
+    </>
   );
 }
 
-export default App;
+const MapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+
+export default connect(MapStateToProps)(App);
